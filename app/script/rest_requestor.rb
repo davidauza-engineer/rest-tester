de3e2f9218f1de915916b953ef1987fee0d0698a
davidauza.engineer <davidauza01@gmail.com>
2020-01-17 11:11:18 -0500

    Finish project

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,4 +1,11 @@
 require 'rest-client'
 url = "http://localhost:3000/users"
+url_two = "http://localhost:3000/users/new"
+url_three = "http://localhost:3000/users/1"
+url_four = "http://localhost:3000/users/1/edit"
 
-puts RestClient.get(url)
+puts RestClient.get(url) # index
+puts RestClient.get(url_two) # new
+puts RestClient.get(url_three) # show
+puts RestClient.get(url_four) # edit
+puts RestClient.post(url, "")
