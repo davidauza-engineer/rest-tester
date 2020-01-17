9ede91ae6bae1470b71f37f8381ec1197ce18a25
Luis Novoa <luis.matte.novoa@gmail.com>
2020-01-17 11:51:01 -0400

    Created methods for the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @name = "I'm the index action"
+  end
+
+  def show
+    @name = "I'm the show action"
+  end
+
+  def edit
+    @name = "I'm the edit action"
+  end
+
+  def new
+    @name = "I'm the new action"
+  end
 end
